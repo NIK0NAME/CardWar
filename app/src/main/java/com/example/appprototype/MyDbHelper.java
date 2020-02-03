5b282f501c8c923dc28275d23f459ed3300a5e11
Nikolay Zabaleta <n.zabaleta@e-coordina.com>
2020-02-03 16:23:32 +0100

    db helper definido

---
+++ app/src/main/java/com/example/appprototype/MyDbHelper.java
@@ -0,0 +1,25 @@
+package com.example.appprototype;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+
+
+    public MyDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
